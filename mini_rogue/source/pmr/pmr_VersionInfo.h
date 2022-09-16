a2b358cab6740140e92cdadeced5acd3d41f16b7
R2Road <studior2road@gmail.com>
2022-09-16 23:48:49 +0900

    update struct : pmr - VersionInfo

--- mini_rogue/source/pmr/pmr_VersionInfo.h
+++ mini_rogue/source/pmr/pmr_VersionInfo.h
@@ -12,11 +12,18 @@ namespace pmr
 	static struct
 	{
 
-		const char VersionNumber_1 = '1';
+		const char VersionNumber_1 = '0';
 		const char VersionNumber_2 = '0';
-		const char VersionNumber_3 = '4';
+		const char VersionNumber_3 = '0';
 
 		const char String4Version[19] = { 'T', 'i', 'm', 'e', 'T', 'i', 'm', 'e', 'r', ' ', ':', ' ', 'v', VersionNumber_1, '.', VersionNumber_2, '.', VersionNumber_3, '\0' };
+
+		const char* const String4Road2Version_0_0_0 = {
+			"### Road 2 Version 0.0.0 ###"
+			"\n"
+			"\n"	"[o] " "Make Project"
+			"\n"	"[o] " "Cleanup"
+		};
 
 	} VersionInfo;
 }
